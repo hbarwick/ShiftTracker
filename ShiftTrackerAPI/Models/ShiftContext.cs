85977977b764ff1ac256137fd008c588566094da
hbarwick <hallambarwick@hotmail.com>
2022-08-23 13:42:10 +0100

    Add project files.

---
+++ ShiftTrackerAPI/Models/ShiftContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace ShiftTrackerAPI.Models
+{
+    public class ShiftContext : DbContext
+    {
+        public ShiftContext(DbContextOptions<ShiftContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Shift> Shifts { get; set; } = null!;
+
+        //public string DbPath { get; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //    => options.UseSqlServer(@"Data Source=(localdb)\localdb;Integrated Security=true;Database=Shifts");
+
+    }
+}
